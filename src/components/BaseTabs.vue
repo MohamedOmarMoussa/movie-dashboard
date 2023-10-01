4b61bde701a1c588d6ca9cbea0361df1b3032f2f
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2023-10-01 21:01:16 +0200

    modif ok

--- src/components/BaseTabs.vue
+++ src/components/BaseTabs.vue
@@ -18,12 +18,12 @@ export default defineComponent({
 
 <template>
   <div class="">
-    <ul class="">
+    <ul class="grid grid-cols-2 md:grid-cols-5 mx-auto text-sm md:text-lg">
       <li
         v-for="(tab, index) in tabs"
         :key="index"
         :class="{ 'border-red-600': tab === selectedTab }"
-        class="cursor-pointer py-2 px-4 lg:inline-flex transition hover:text-primary ease-linear text-lg"
+        class="cursor-pointer py-2 px-4 transition hover:text-primary ease-linear"
         @click="$emit('changeTab', tab)"
       >
         {{ tab.name }}
